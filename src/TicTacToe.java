f0f2173d9f1e3112e42af91588d3853168df0004
Nabin Rai <rainabinkhyal@gmail.com>
2023-02-04 15:21:17 +0545

    Skeleton update
    first step

---
+++ src/TicTacToe.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener{
+
+	TicTacToe(){
+		
+	}
+	
+	
+		
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void firstTurn() {
+		
+	}
+	
+	public void check() {
+		
+	}
+	public void xWins(int a, int b, int c) {
+		
+	}
+	public void yWins(int a, int b, int c) {
+		
+	}
+	
+	
+	
+}
